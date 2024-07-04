3d5223d45f87832e5fb0299d5f52cd35d8170404
HP <rejoicemawusi57@gmail.com>
2024-07-04 05:47:16 -0700

    adding all files

---
+++ src/rejoice.js
@@ -0,0 +1,37 @@
+let learn = document.getElementById("learn");
+learn.style.backgroundColor ="blue";
+learn.style.color ="white";
+learn.innerHTML ="I can now use DOM to manipulate JS💪🏾";
+
+let list = document.querySelector(".dom");
+list.style.backgroundColor="grey";
+list.style.color="white";
+list.style.border="7px solid orange";
+list.innerHTML="I now know what the DOM is 🙌";
+
+let your = document.querySelector(".access");
+your.style.backgroundColor="orange";
+your.style.color="black";
+your.style.border="7px solid burlywood";
+your.innerHTML="I now know how to access / select elements in the DOM👊"
+
+let second = document.getElementById("method");
+second.style.backgroundColor="brown";
+second.style.color="white";
+second.style.border="7px solid indigo"
+second.innerHTML="I now know how to use the style method to change the style of an elelment in the DOM✅"
+
+let first = document.querySelector("#differ");
+first.style.backgroundColor="purple";
+first.style.color="white";
+first.style.border="7px solid blue"
+first.innerHTML="I now know how the styling properties differ in JS from CSS👍🏻";
+
+let fest = document.getElementById("excited");
+fest.style.backgroundColor="black";
+fest.style.color="white";
+fest.style.border="7px solid pink";
+fest.innerHTML="Yes i did it!! I manipulated the DOM and styled my elements😍😍😍";
+
+
+
